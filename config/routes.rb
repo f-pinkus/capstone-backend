f8625103cc8fe8c06a4a327b2a534e0a61373621
f-pinkus <faigypinkus@gmail.com>
2025-08-07 22:35:44 -0400

    add users model and signup action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :recipes
+
+  post "/signup" => "users#create"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_05_003959) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_08_022739) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,6 +21,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_05_003959) do
     t.text "instructions"
     t.string "difficulty"
     t.string "photo_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
