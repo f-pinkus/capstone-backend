3bbe24d434f6dbd23c1b8099b931f1120a2332af
f-pinkus <faigypinkus@gmail.com>
2025-08-07 22:39:00 -0400

    add cookies helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::API
-    protect_from_forgery with: :exception, unless: -> { request.format.json? }
+  protect_from_forgery with: :exception, unless: -> { request.format.json? }
+
+  helper_method :current_user
+
+  skip_before_action :verify_authenticity_token
+
+  def current_user
+    User.find_by(id: cookies.signed[:user_id])
+  end
+
+  def authenticate_user
+    unless current_user
+      render json: {}, status: :unauthorized
+    end
+  end
 end
