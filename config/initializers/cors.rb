e825e25ac9a371448aef57b640f44f1875a7aad4
f-pinkus <faigypinkus@gmail.com>
2026-01-05 22:46:44 -0500

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "localhost:5173",
-    "localhost:4173", "https://biteshare1.netlify.app"
+    "localhost:4173", "https://getbiteshare.netlify.app"
     resource "*",
       headers: :any,
       methods: [:get, :post, :delete, :patch, :put, :options],
